59765ad648abb11d2c57bb750716d3e4931035d7
AhsanAli28 <rishabhchauhan072003@gmail.com>
2024-10-09 20:07:26 +0500

    Enemy movement Added for Follow
    Enemy movement Added for Follow player

--- Assets/Script/Character/Enemy/Enemy.cs
+++ Assets/Script/Character/Enemy/Enemy.cs
@@ -1,33 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Events;
-using UnityEngineInternal;
 
 public class Enemy : Character
 {
-    public UnityEvent<Vector2> OnMovementInput;
-
     public UnityEvent OnAttack;
 
     [SerializeField] private Transform player;
-
-    [SerializeField] private float chaseDistance = 3f;
-
-    [SerializeField] private float attackDistance = 0.8f;
-
+    [SerializeField] private float chaseDistance = 3f;       // Distance within which the enemy chases the player
+    [SerializeField] private float attackDistance = 0.8f;    // Distance within which the enemy attacks the player
     [Header("Attack")]
     public float meleeAttackDamage;
     public LayerMask playerLayer;
     public float AttackCooldownDuration = 2f;
 
-    private bool isAttack = true;
-
+    private bool isAttackCooldown = false;
+    private NavMeshAgent agent;
     private SpriteRenderer sr;
 
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
         sr = GetComponent<SpriteRenderer>();
+
+        // Disable automatic rotation
+        agent.updateRotation = false;
     }
 
     private void Update()
@@ -37,42 +36,62 @@ public class Enemy : Character
 
         float distance = Vector2.Distance(player.position, transform.position);
 
-        if (distance < chaseDistance) // less than chase distance
+        if (distance <= chaseDistance)
         {
-            if (distance <= attackDistance) //if the player is in the chase distance
+            if (distance <= attackDistance && !isAttackCooldown)
             {
-                //attack player
-                OnMovementInput?.Invoke(Vector2.zero);
-                Debug.Log("Attacked");
-                OnAttack?.Invoke();
-                //check if player rotate
-                float x=player.position.x - transform.position.x;
-                if (x > 0)
-                {
-                    sr.flipX = true;
-                }
-                else
-                {
-                    sr.flipX = false;
-                }
-
+                // Attack player
+                StartCoroutine(Attack());
             }
             else
             {
-                //chase player
-                Vector2 direction = player.position - transform.position;
-                OnMovementInput?.Invoke(direction.normalized); //transport move directions to enemy controller script
+                // Move towards player
+                agent.SetDestination(player.position);
+                FacePlayer();
             }
         }
-        else {
-            //giveup chasing
-            OnMovementInput?.Invoke(Vector2.zero);
+        else
+        {
+            agent.SetDestination(transform.position); // Stop moving
         }
+
+        // Fix the X rotation while allowing Y and Z rotations to change
+        FixXRotation();
+    }
+
+    private IEnumerator Attack()
+    {
+        OnAttack?.Invoke();
+        Debug.Log("Attacking player");
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, attackDistance, playerLayer);
+        foreach (Collider2D hitCollider in hitColliders)
+        {
+            hitCollider.GetComponent<Character>().TakeDamage(meleeAttackDamage);
+        }
+
+        isAttackCooldown = true;
+        yield return new WaitForSeconds(AttackCooldownDuration);
+        isAttackCooldown = false;
+    }
+
+    private void FacePlayer()
+    {
+        // Determine the direction to the player
+        float x = player.position.x - transform.position.x;
+
+        // Flip the sprite based on the player's position
+        sr.flipX = x > 0; // Flip if the player is to the right
+    }
+
+    private void FixXRotation()
+    {
+        // Fix the X rotation while allowing Y and Z to rotate freely
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 
     public void MeleeAttackAnimEvent()
     {
-        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, attackDistance,playerLayer);
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, attackDistance, playerLayer);
 
         foreach (Collider2D hitCollider in hitColliders)
         {
@@ -83,6 +102,8 @@ public class Enemy : Character
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position,attackDistance);
+        Gizmos.DrawWireSphere(transform.position, attackDistance); // Attack distance visualization
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance); // Chase distance visualization
     }
 }
