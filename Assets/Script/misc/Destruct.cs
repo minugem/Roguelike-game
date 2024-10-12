968c633812510823baac17034d1032b627d04299
PineappleBearr <170580290+PineappleBearr@users.noreply.github.com>
2024-10-12 16:36:20 +1300

    Update on Destruct script
    Add Desrtructive layer and c# code, now a barrel on the map will be destructed after player attack

--- Assets/Script/misc/Destruct.cs
+++ Assets/Script/misc/Destruct.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructable : MonoBehaviour
+public class Destruct : MonoBehaviour
 {
     public GameObject destoryVFX; //VFX after destoryed
 
